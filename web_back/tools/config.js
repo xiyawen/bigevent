3f9db9711141cc331cfdeec48fb6b44551eeb351
xi <617426726@qq.com>
2019-12-15 17:31:23 +0800

    用户登录和退出

---
+++ web_back/tools/config.js
@@ -0,0 +1,5 @@
+// 用来配置一些变量
+var BASEURL = "http://localhost:8000";
+
+var LOGIN = BASEURL + "/admin/login";
+var LOGOUT = BASEURL + "/admin/logout";
---
+++ web_back/tools/user.js
@@ -0,0 +1,36 @@
+var user = {
+    // login
+    login: function (options) {
+        $.ajax({
+            type: "POST",
+            url: LOGIN,
+            data: {
+                user_name: options.data.user_name,
+                password: options.data.password,
+            },
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    },
+    // logout
+    logout: function (options) {
+        $.ajax({
+            type: "POST",
+            url: LOGOUT,
+            success: function (res) {
+                console.log(res);
+
+                if (res.code === 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    }
+}
